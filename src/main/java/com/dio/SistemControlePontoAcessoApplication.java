5b18cfcd4ef9f54e2db8fb55d0712d9df558ced5
Matheus Dario <dario.matheus97@gmail.com>
2021-08-21 19:53:24 -0300

    projeto-iniciado

---
+++ src/main/java/com/dio/SistemControlePontoAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemControlePontoAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemControlePontoAcessoApplication.class, args);
+	}
+
+}
